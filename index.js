bb3b1db1a6edd2a7f6fad7bbcd5e49dbe8f5577a
viyk <57522684+viyk@users.noreply.github.com>
2022-09-02 19:40:13 +0800

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 let users = [
   {
     id: 1,
@@ -71,9 +73,49 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/users', (request, response) => {
+app.get('/api/users', (request, response) => {
+  // console.log(request.headers);
   response.json(users)
 })
+
+app.get('/api/users/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const user = users.find(user => user.id === id)
+  if (user) {
+    response.json(user)
+  } else {
+    response.statusMessage = `User Not Found (id: ${id})`
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/users/:id', (request, response) => {
+  const id = Number(request.params.id)
+  users = users.filter(user => user.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/users', (request, response) => {
+  const body = request.body
+  // 检验数据规范性
+  if (!body.username || !body.password) {
+    return response.status(400).json({ error: '用户信息不完整' })
+  }
+
+  const user = {
+    ...body,
+    groups: body.groups || [1],
+    id: generateId()
+  }
+  users = users.concat(user)
+  response.json(user)
+})
+
+const generateId = () => {
+  const maxId = users.length > 0 ? Math.max(...users.map(user => user.id)) : 0
+  return maxId + 1
+}
 
 const PORT = 3001
 app.listen(PORT, () => {
