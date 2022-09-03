3a71126bce02912834fa22981eb1d1c4747326b6
viyk <57522684+viyk@users.noreply.github.com>
2022-09-03 15:45:54 +0800

    add models

--- index.js
+++ index.js
@@ -1,86 +1,80 @@
+require('dotenv').config()
+const { request } = require('express')
+const { response } = require('express')
+// console.log(process.env.DATABASE_URL)
 const express = require('express')
-
+const { Sequelize, QueryTypes, DataTypes, Model } = require('sequelize')
 const app = express()
 
-app.use(express.json())
-
-let users = [
-  {
-    id: 1,
-    username: "joey",
-    display_name: "Joey",
-    password: "1234",
-    email: "a@b.com",
-    groups: [
-      1
-    ]
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+// 模型
+class User extends Model { }
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
-    id: 2,
-    username: "bob",
-    display_name: "Bob",
-    password: "1234",
-    email: "a@b.com",
-    groups: [
-      1
-    ]
+  name: {
+    type: DataTypes.STRING(30),
+    allowNull: false
   },
-  {
-    id: 3,
-    username: "alice",
-    display_name: "Alice",
-    password: "1234",
-    email: "a@b.com",
-    groups: [
-      2
-    ]
+  displayName: {
+    type: DataTypes.STRING(30)
   },
-  {
-    username: "pop",
-    password: "9876",
-    email: "",
-    groups: "",
-    display_name: "航",
-    id: 6
+  email: {
+    type: DataTypes.STRING(30)
   },
-  {
-    username: "12321",
-    password: "111",
-    email: "",
-    groups: "",
-    display_name: "lkj12",
-    id: 7
-  },
-  {
-    username: "a",
-    password: "111",
-    email: "",
-    groups: "",
-    display_name: "Abc",
-    id: 8
-  },
-  {
-    username: "w",
-    password: "111",
-    email: "",
-    groups: "",
-    display_name: "wer",
-    id: 9
+  password: {
+    type: DataTypes.STRING(30)
   }
-]
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: true,
+  createdAt: 'createdAt',
+  updatedAt: false,
+  modelName: 'user'
+})
+
+class Group extends Model { }
+Group.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING(30),
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: true,
+  createdAt: 'createdAt',
+  updatedAt: false,
+  modelName: 'group'
+})
+
+app.use(express.json())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/users', (request, response) => {
-  // console.log(request.headers);
+// users
+app.get('/api/users', async (request, response) => {
+  const users = await User.findAll()
   response.json(users)
 })
 
-app.get('/api/users/:id', (request, response) => {
+app.get('/api/users/:id', async (request, response) => {
   const id = Number(request.params.id)
-  const user = users.find(user => user.id === id)
+  const user = await User.findOne({where:{id:id}})
   if (user) {
     response.json(user)
   } else {
@@ -89,35 +83,40 @@ app.get('/api/users/:id', (request, response) => {
   }
 })
 
-app.delete('/api/users/:id', (request, response) => {
+app.delete('/api/users/:id', async (request, response) => {
   const id = Number(request.params.id)
-  users = users.filter(user => user.id !== id)
+  User.destroy({
+    where: {
+      id: id
+    }
+  })
 
   response.status(204).end()
 })
 
-app.post('/api/users', (request, response) => {
+app.post('/api/users', async (request, response) => {
   const body = request.body
   // 检验数据规范性
-  if (!body.username || !body.password) {
+  if (!body.name || !body.password) {
     return response.status(400).json({ error: '用户信息不完整' })
   }
 
-  const user = {
-    ...body,
-    groups: body.groups || [1],
-    id: generateId()
-  }
-  users = users.concat(user)
+  const user = await User.create(body)
   response.json(user)
 })
 
-const generateId = () => {
-  const maxId = users.length > 0 ? Math.max(...users.map(user => user.id)) : 0
-  return maxId + 1
-}
+// TODO update user
+// app.put('/api/users/:id', async (request, response)=> {
+//   response.status(200).end()
+// })
 
-const PORT = 3001
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3001
+app.listen(PORT, async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('成功连接数据库')
+  } catch (error) {
+    console.error('数据库连接失败:', error)
+  }
   console.log(`Server running on port ${PORT}`)
 })
